49eeec114a8177ce05b75cb5488cca560d55c26e
Eronavrp <37598877+Eronavrp@users.noreply.github.com>
2020-05-20 23:06:44 +0200

    json

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var getUserMedia = require("getusermedia");
-console.log("hej");
 getUserMedia({ video: true, audio: false }, function (err, stream) {
 	if (err) return console.error(err);
 
@@ -14,18 +13,39 @@ getUserMedia({ video: true, audio: false }, function (err, stream) {
 		document.getElementById("yourId").value = JSON.stringify(data);
 	});
 
+	var myJSON = '{"destination":"John", "value":31}'
+
 	document.getElementById("connect").addEventListener("click", function () {
 		var otherId = JSON.parse(document.getElementById("otherId").value);
 		peer.signal(otherId);
 	});
 
 	document.getElementById("send").addEventListener("click", function () {
-		var yourMessage = document.getElementById("yourMessage").value;
-		peer.send(yourMessage);
+		var myObj = JSON.parse(myJSON);
+		myObj.destination="message";
+		myObj.value=document.getElementById("yourMessage").value+"";
+		m=JSON.stringify(myObj);
+		peer.send(m);
+	});
+
+	document.getElementById("generate").addEventListener("click", function () {
+		var myObj = JSON.parse(myJSON);
+		myObj.destination="result";
+		var random=Math.random()*10;
+		myObj.value=random+"";
+		m=JSON.stringify(myObj);
+		peer.send(m);
 	});
 
 	peer.on("data", function (data) {
-		document.getElementById("messages").textContent += data + "\n";
+		var d=JSON.parse(data);
+		if(d.destination=="message"){
+			document.getElementById("messages").textContent += d.value + "\n";
+		}
+		else{
+			document.getElementById('result').innerHTML=d.value;
+		}
+		
 	});
 
 	peer.on("stream", function (stream) {
@@ -33,6 +53,6 @@ getUserMedia({ video: true, audio: false }, function (err, stream) {
 		document.body.appendChild(video);
 		video.srcObject = stream;
 		//video.src = window.URL.createObjectURL(stream);
-		video.play();
+		//video.play();
 	});
 });
